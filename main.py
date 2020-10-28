51c567c5289107cd034b2c400471f68dcd5b9f52
The-Reflex <70115287+The-Reflex@users.noreply.github.com>
2020-10-28 13:50:19 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+print("Welcome to Conversh like Connery")
+
+
+repeat = True
+
+while repeat:
+    usertext = input("Enter what you want to be converted into Connery:")
+
+    result = usertext.replace("s","sh")
+    result = result.replace("S","Sh")
+    print(result)
+
+    yesno = input("Do you wish to quit? (Yes/No):")
+    if yesno == "No":
+        repeat = True
+
+    if yesno == "No":
+        repeat = True
+
+    if yesno == "Yes":
+        repeat = False
+
+    if yesno == "yes":
+        repeat = False
